61a11f8c58da3927771329429f77f538e61a40a2
Ali Kutlusoy <ali@kutlusoy.com>
2026-06-17 10:14:42 +0200

    Update db.h

--- db.h
+++ db.h
@@ -12,7 +12,7 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70017
+#define REQUIRE_VERSION 70016
 
 extern int nMinimumHeight;
 static inline int GetRequireHeight(const bool testnet = fTestNet)
